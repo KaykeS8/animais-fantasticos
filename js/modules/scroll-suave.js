55485fae65a77f6866f86bd87bdfb9e2ff10cf21
kayke <kayke.py@gmail.com>
2021-12-18 20:50:28 -0300

    Novo javascript criado com modulos e exportados, esta é a parte do scroll suave do link

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,20 @@
+export default function initScrollSection() {
+
+    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]')
+
+    function scrollToSection(e) {
+        e.preventDefault()
+        const href = e.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        const topo = section.offsetTop;
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    })
+}
