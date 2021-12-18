3d75f1e583fd0f9468e09e010b63b6a1d6795355
kayke <kayke.py@gmail.com>
2021-12-18 20:45:40 -0300

    esta pasta é onde esta presente todo codigo importado

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSection from './modules/scroll-suave.js'
+import initScrollWindow from './modules/animacao-scroll.js'
+import initTabNav from './modules/tabNav.js'
+import initAccordion from './modules/accordion.js'
+
+initScrollSection()
+initScrollWindow()
+initTabNav()
+initAccordion()
