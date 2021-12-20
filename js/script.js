5b247580eaf579ba521c02fb16c48f3ac013910f
kayke <kayke.py@gmail.com>
2021-12-20 15:26:24 -0300

    Executando a função do modal.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSection from './modules/scroll-suave.js'
 import initScrollWindow from './modules/animacao-scroll.js'
 import initTabNav from './modules/tabNav.js'
 import initAccordion from './modules/accordion.js'
+import initModal from './modules/modal.js'
 
 initScrollSection()
 initScrollWindow()
 initTabNav()
 initAccordion()
+initModal()
