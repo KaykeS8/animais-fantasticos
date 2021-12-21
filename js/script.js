3265fcf783fb078262938cc00af466ab52d0fe37
kayke <kayke.py@gmail.com>
2021-12-21 12:27:08 -0300

    Arquivo javascript onde esta importado todos os modulos js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollWindow from './modules/animacao-scroll.js'
 import initTabNav from './modules/tabNav.js'
 import initAccordion from './modules/accordion.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initScrollSection()
 initScrollWindow()
 initTabNav()
 initAccordion()
 initModal()
+initTooltip()
