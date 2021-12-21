b0b7a6a6787bbcd1368127eb150d8a2194973475
kayke <kayke.py@gmail.com>
2021-12-21 18:49:40 -0300

    Importando o codigo do outside para este arquivo

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,18 +1,19 @@
+import outsideClick from "./outside.js";
+
 export default function initDropDownMenu() {
+    const dropDownMenus = document.querySelectorAll('[data-dropdown]')
 
-}
+    dropDownMenus.forEach((item) => {
+        ['touchstart', 'click'].forEach((userEvent => {
+            item.addEventListener(userEvent, handleClick)
+        }))
+    });
 
-const dropDownMenus = document.querySelectorAll('[data-dropdown]')
-
-dropDownMenus.forEach((item) => {
-    ['touchstart', 'click'].forEach((userEvent => {
-        item.addEventListener(userEvent, handleClick)
-    }))
-});
-
-function handleClick(e) {
-    e.preventDefault()
-    console.log(e)
-    this.classList.toggle('active')
-}
-
+    function handleClick(e) {
+        e.preventDefault()
+        this.classList.toggle('active')
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active')
+        })
+    };
+};
