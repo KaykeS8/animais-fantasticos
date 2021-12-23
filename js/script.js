ec8322df719c7344f810633a39f7613fa2a142ad
kayke <kayke.py@gmail.com>
2021-12-22 21:05:55 -0300

    Arquivo onde esta presente todas as importações do modulos de javascript do site

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initAccordion from './modules/accordion.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropDownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu-mobile.js'
 
 initScrollSection()
 initScrollWindow()
@@ -13,3 +14,4 @@ initAccordion()
 initModal()
 initTooltip()
 initDropDownMenu()
+initMenuMobile()
