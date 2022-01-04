4b51d7e725599253c9c263a12a24875b65b63510
kayke <kayke.py@gmail.com>
2022-01-04 09:42:29 -0300

    Importando uma nova funcionalidade.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ import initTooltip from './modules/tooltip.js'
 import initDropDownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initAnimaNumero from './modules/anima-numeros.js'
+import initFuncinamento from './modules/funcionamento.js'
 
 initScrollSection()
 initScrollWindow()
@@ -17,3 +18,4 @@ initTooltip()
 initDropDownMenu()
 initMenuMobile()
 initAnimaNumero()
+initFuncinamento()
