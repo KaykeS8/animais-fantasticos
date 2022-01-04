5a4f58d97b18c4869ca7922b6a55eb04271c7122
kayke <kayke.py@gmail.com>
2022-01-04 08:11:49 -0300

    Importando a nova função javascript

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropDownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumero from './modules/anima-numeros.js'
 
 initScrollSection()
 initScrollWindow()
@@ -15,3 +16,4 @@ initModal()
 initTooltip()
 initDropDownMenu()
 initMenuMobile()
+initAnimaNumero()
