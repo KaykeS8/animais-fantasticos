42a022d7cb5a8f2d01decaf37a1f229fbefeea71
kayke <kayke.py@gmail.com>
2022-01-04 09:40:01 -0300

    Script onde esta presente a função de horario de funcionamento da loja

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,24 @@
+export default function initFuncinamento() {
+    
+    const funcionamento = document.querySelector('[data-semana]')
+    const diasSemanas = funcionamento.dataset.semana.split(',').map(Number)
+    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
+    
+    
+    
+    const dataAgora = new Date()
+    const diaAgora = dataAgora.getDay()
+    const horasAgora = dataAgora.getHours()
+    
+    
+    
+    const semanaAberta = diasSemanas.indexOf(diaAgora) !== -1
+    const horarioAberto = (horasAgora >= horarioSemana[0] && horasAgora < horarioSemana[1])
+    
+    if(semanaAberta && horarioAberto) {
+        funcionamento.classList.add('aberto')
+    } else {
+        funcionamento.classList.add('fechado')
+    }
+
+}
