f78afc6a1399d1f71311e81b85663a968078206e
kayke <kayke.py@gmail.com>
2022-01-18 12:17:41 -0300

    Arquivo javascript responsavel pela requisição de uma api de bitcoint para mostrar na aplicação

---
+++ js/modules/bitcoint-Animais.js
@@ -0,0 +1,14 @@
+export default function initFecthBitcoint() {
+    async function puxarBitcoint(url) {
+        try {
+            const bitcointResponse = await fetch(url)
+            const bitcointJson = await bitcointResponse.json()
+            const preco = document.querySelector('.btc-preco')
+            preco.innerText = (1000 / bitcointJson.BRL.sell).toFixed(4)
+        } catch (erro) {
+            console.log(erro)
+        }
+    }
+    puxarBitcoint('https://blockchain.info/ticker')
+}
+
