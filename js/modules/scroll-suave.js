a0de2518582efc458a3ad7f387e872f09554dc21
kayke <kayke.py@gmail.com>
2022-03-09 18:18:13 -0300

    Animação do scroll da pagina feito com javascript puri

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,20 +1,18 @@
 export default function initScrollSection() {
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]')
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
 
-    function scrollToSection(e) {
-        e.preventDefault()
-        const href = e.currentTarget.getAttribute('href')
-        const section = document.querySelector(href)
-        const topo = section.offsetTop;
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
 
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
