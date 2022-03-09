e52eb3a713159ce06de829a94c5bbbd2a6a5ea69
kayke <kayke.py@gmail.com>
2022-03-09 18:10:33 -0300

    Arquivo javascript com uma requisição via fetch de uma API de vitcoint

--- js/modules/bitcoint-Animais.js
+++ js/modules/bitcoint-Animais.js
@@ -1,14 +1,13 @@
 export default function initFecthBitcoint() {
-    async function puxarBitcoint(url) {
-        try {
-            const bitcointResponse = await fetch(url)
-            const bitcointJson = await bitcointResponse.json()
-            const preco = document.querySelector('.btc-preco')
-            preco.innerText = (1000 / bitcointJson.BRL.sell).toFixed(4)
-        } catch (erro) {
-            console.log(erro)
-        }
+  async function puxarBitcoint(url) {
+    try {
+      const bitcointResponse = await fetch(url);
+      const bitcointJson = await bitcointResponse.json();
+      const preco = document.querySelector('.btc-preco');
+      preco.innerText = (1000 / bitcointJson.BRL.sell).toFixed(4);
+    } catch (erro) {
+      console.log(erro);
     }
-    puxarBitcoint('https://blockchain.info/ticker')
+  }
+  puxarBitcoint('https://blockchain.info/ticker');
 }
-
