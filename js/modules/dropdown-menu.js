ba56b08435d84028f036ee3f92786411c04a0239
kayke <kayke.py@gmail.com>
2022-03-09 18:11:51 -0300

    Animação de um dropdow com javascript

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,19 @@
-import outsideClick from "./outside.js";
+import outsideClick from './outside.js';
 
 export default function initDropDownMenu() {
-    const dropDownMenus = document.querySelectorAll('[data-dropdown]')
+  const dropDownMenus = document.querySelectorAll('[data-dropdown]');
 
-    dropDownMenus.forEach((item) => {
-        ['touchstart', 'click'].forEach((userEvent => {
-            item.addEventListener(userEvent, handleClick)
-        }))
+  function handleClick(e) {
+    e.preventDefault();
+    this.classList.toggle('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
     });
+  }
 
-    function handleClick(e) {
-        e.preventDefault()
-        this.classList.toggle('active')
-        outsideClick(this, ['touchstart', 'click'], () => {
-            this.classList.remove('active')
-        })
-    };
-};
+  dropDownMenus.forEach((item) => {
+    ['touchstart', 'click'].forEach(((userEvent) => {
+      item.addEventListener(userEvent, handleClick);
+    }));
+  });
+}
