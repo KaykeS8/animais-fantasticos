4a96fd0289ca112b6f090ea3919d3c9d5fc4c770
kayke <kayke.py@gmail.com>
2022-03-09 18:07:16 -0300

    Arquivo javascript com a funcionalidade de animar o scroll da página

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,21 +1,22 @@
 export default function initScrollWindow() {
-    const sections = document.querySelectorAll('[data-anime="scroll"')
+  const sections = document.querySelectorAll('[data-anime="scroll"');
+  const windowMetade = window.innerHeight * 0.6;
 
-    if (sections.length) {
-        const windowMetade = window.innerHeight * 0.6
-        function animarScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top
-                const isSectionVisible = (sectionTop - windowMetade) < 0
-                const ativar = 'ativo'
-                if (isSectionVisible) {
-                    section.classList.add(ativar)
-                } else if(section.classList.contains(ativar)) {
-                    section.classList.remove(ativar)
-                }
-            });
-        };
-        animarScroll();
-        window.addEventListener('scroll', animarScroll)
-    };
-};
+  function animarScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      const ativar = 'ativo';
+      if (isSectionVisible) {
+        section.classList.add(ativar);
+      } else if (section.classList.contains(ativar)) {
+        section.classList.remove(ativar);
+      }
+    });
+  }
+
+  if (sections.length) {
+    animarScroll();
+    window.addEventListener('scroll', animarScroll);
+  }
+}
