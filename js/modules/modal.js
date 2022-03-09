9841ce502b6f8e2d9d2331093bc29f75491fd7fa
kayke <kayke.py@gmail.com>
2022-03-09 18:16:13 -0300

    Modal feito com javascript

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,34 +1,28 @@
 export default function initModal() {
-    const botaoAbrir = document.querySelector('[data-modal="abrir"]')
-    const botaoFechar = document.querySelector('[data-modal="fechar"]')
-    const containerModal = document.querySelector('[data-modal="container"]')
-    const cadastrouModal = document.querySelector('[data-modal="login"]')
+  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+  const botaoFechar = document.querySelector('[data-modal="fechar"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+  const cadastrouModal = document.querySelector('[data-modal="login"]');
 
-    if (botaoAbrir && botaoFechar && containerModal && cadastrouModal) {
-        function abrirModal(e) {
-            e.preventDefault()
-            containerModal.classList.add('ativo')
-        }
+  function abrirModal(e) {
+    e.preventDefault();
+    containerModal.classList.add('ativo');
+  }
 
-        function fecharModal(e) {
-            e.preventDefault()
-            containerModal.classList.remove('ativo')
-        }
+  function fecharModal(e) {
+    e.preventDefault();
+    containerModal.classList.remove('ativo');
+  }
 
-
-        function cliqueForaDoModal(e) {
-            if (e.target === this) {
-                fecharModal(e)
-            }
-        }
-
-        function cadastrou() {
-            alert('Você cadastrou com sucesso')
-        }
-
-        botaoAbrir.addEventListener('click', abrirModal)
-        botaoFechar.addEventListener('click', fecharModal)
-        containerModal.addEventListener('click', cliqueForaDoModal)
-        cadastrouModal.addEventListener('click', cadastrou)
+  function cliqueForaDoModal(e) {
+    if (e.target === this) {
+      fecharModal(e);
     }
+  }
+
+  if (botaoAbrir && botaoFechar && containerModal && cadastrouModal) {
+    botaoAbrir.addEventListener('click', abrirModal);
+    botaoFechar.addEventListener('click', fecharModal);
+    containerModal.addEventListener('click', cliqueForaDoModal);
+  }
 }
