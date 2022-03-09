dccd032bd1ec4ee7a4418a59f1e62f6feb60cec6
kayke <kayke.py@gmail.com>
2022-03-09 18:02:52 -0300

    Código com a funcionalidade do accordion do site

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,14 +1,14 @@
 export default function initAccordion() {
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt')
-    if (accordionList.length) {
-        const classAtivo = 'ativo';
-        accordionList[0].nextElementSibling.classList.add(classAtivo)
-        function activeAccordion() {
-            this.classList.toggle(classAtivo)
-            this.nextElementSibling.classList.toggle(classAtivo)
-        }
-        accordionList.forEach((item) => {
-            item.addEventListener('click', activeAccordion)
-        })
-    };
-};
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const classAtivo = 'ativo';
+  function activeAccordion() {
+    this.classList.toggle(classAtivo);
+    this.nextElementSibling.classList.toggle(classAtivo);
+  }
+  if (accordionList.length) {
+    accordionList[0].nextElementSibling.classList.add(classAtivo);
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
