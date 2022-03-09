e63f94fda3cd224f71a555a11edd0dc99ee4ee05
kayke <kayke.py@gmail.com>
2022-03-09 18:17:12 -0300

    Outside desenvolvido com javascript vanilla

--- js/modules/outside.js
+++ js/modules/outside.js
@@ -1,21 +1,22 @@
 export default function outsideClick(element, events, callback) {
-    const html = document.documentElement
-    const outside = 'data-outside'
+  const html = document.documentElement;
+  const outside = 'data-outside';
 
-    if (!element.hasAttribute(outside)) {
-        events.forEach((userEvent) => {
-            setTimeout(() => { html.addEventListener(userEvent, handleOutSideClick) })
-        })
-        element.setAttribute(outside, '')
+  function handleOutSideClick(e) {
+    if (!element.contains(e.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.addEventListener(userEvent, handleOutSideClick);
+      });
+      html.removeEventListener('click', handleOutSideClick);
+      callback();
     }
-    function handleOutSideClick(e) {
-        if (!element.contains(e.target)) {
-            element.removeAttribute(outside)
-            events.forEach((userEvent) => {
-                html.addEventListener(userEvent, handleOutSideClick)
-            })
-            html.removeEventListener('click', handleOutSideClick)
-            callback()
-        }
-    }
+  }
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => { html.addEventListener(userEvent, handleOutSideClick); });
+    });
+    element.setAttribute(outside, '');
+  }
 }
