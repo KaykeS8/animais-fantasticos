2f1cb77294ea42c618cf1a4f80dbaae69aa7e7d9
Kayke simão <kayke.simao@gmail.com>
2022-03-12 09:56:59 -0300

    Refatoração

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-import initScrollSection from './modules/scroll-suave.js'
-import initScrollWindow from './modules/animacao-scroll.js'
-import initTabNav from './modules/tabNav.js'
-import initAccordion from './modules/accordion.js'
-import initModal from './modules/modal.js'
-import initTooltip from './modules/tooltip.js'
-import initDropDownMenu from './modules/dropdown-menu.js'
-import initMenuMobile from './modules/menu-mobile.js'
-import initFuncinamento from './modules/funcionamento.js'
-import initFetchAnimal from './modules/animais-fetch.js'
-import initFecthBitcoint from './modules/bitcoint-Animais.js'
+import ScrollSuave from './modules/scroll-suave.js';
+import initScrollWindow from './modules/animacao-scroll.js';
+import initTabNav from './modules/tabNav.js';
+import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncinamento from './modules/funcionamento.js';
+import initFetchAnimal from './modules/animais-fetch.js';
+import initFecthBitcoint from './modules/bitcoint-Animais.js';
 
-initScrollSection()
-initScrollWindow()
-initTabNav()
-initAccordion()
-initModal()
-initTooltip()
-initDropDownMenu()
-initMenuMobile()
-initFuncinamento()
-initFetchAnimal()
-initFecthBitcoint()
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+initScrollWindow();
+initTabNav();
+initAccordion();
+initModal();
+initTooltip();
+initDropDownMenu();
+initMenuMobile();
+initFuncinamento();
+initFetchAnimal();
+initFecthBitcoint();
