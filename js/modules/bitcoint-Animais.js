597c3f4cf2585d15a8592b28245793bde9e43862
Kayke simão <kayke.simao@gmail.com>
2022-03-12 18:37:39 -0300

    Refatoração do fetch bitcoint

--- js/modules/bitcoint-Animais.js
+++ js/modules/bitcoint-Animais.js
@@ -6,5 +6,3 @@ export default function initFecthBitcoint(url, target) {
       btcPreco.innerText = (1000 / bitcoint.BRL.sell).toFixed(4);
     }).catch((erro) => console.log(Error(erro)));
 }
-
-// 'https://blockchain.info/ticker'
