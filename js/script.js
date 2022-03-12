2365147e73f30ac3df1ff463f3619745f7998c49
Kayke simão <kayke.simao@gmail.com>
2022-03-12 10:17:35 -0300

    Accordion transformado em class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initScrollWindow from './modules/animacao-scroll.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
@@ -13,7 +13,8 @@ import initFecthBitcoint from './modules/bitcoint-Animais.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAccordion();
+const accordio = new Accordion('[data-anime="accordion"] dt');
+accordio.init();
 
 initScrollWindow();
 initTabNav();
